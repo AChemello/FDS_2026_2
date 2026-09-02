1d43299872cf509b81f59d5c63f4895fff20d68f
AChemello <arthur.chemello@edu.pucrs.br>
2026-09-02 21:46:21 +0000

    add: livrosExec

---
+++ execLivro/src/main/java/a/c/execLivro/ExecLivroApplication.java
@@ -0,0 +1,13 @@
+package a.c.execLivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecLivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecLivroApplication.class, args);
+	}
+
+}
